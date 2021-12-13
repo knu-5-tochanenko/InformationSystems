208e5ce60e759f6b31b5544dabf2b90351d3923d
Tai <justataya@gmail.com>
2021-12-13 04:58:53 +0200

    Add REST service to Search and View Activities

--- frontend/app/src/main/java/com/tochanenko/recipefinder/RecipeDetailsActivity.kt
+++ frontend/app/src/main/java/com/tochanenko/recipefinder/RecipeDetailsActivity.kt
@@ -18,102 +18,13 @@ import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 
 class RecipeDetailsActivity : AppCompatActivity() {
-    private val json = """
-    {
-    "id": "716426",
-    "title": "Cauliflower, Brown Rice, and Vegetable Fried Rice",
-    "readyInMinutes": 30,
-    "servings": 8,
-    "image": "https://spoonacular.com/recipeImages/716426-556x370.jpg",
-    "summary": "Cauliflower, Brown Rice, and Vegetable Fried Rice might be a good recipe to expand your side dish recipe box. Watching your figure? This gluten free, dairy free, lacto ovo vegetarian, and vegan recipe has <b>192 calories</b>, <b>7g of protein</b>, and <b>6g of fat</b> per serving. For <b>$1.12 per serving</b>, this recipe <b>covers 19%</b> of your daily requirements of vitamins and minerals. This recipe serves 8. This recipe from fullbellysisters.blogspot.com has 3689 fans. This recipe is typical of Chinese cuisine. From preparation to the plate, this recipe takes about <b>30 minutes</b>. Head to the store and pick up peas, broccoli, salt, and a few other things to make it today. Overall, this recipe earns an <b>awesome spoonacular score of 100%</b>. Users who liked this recipe also liked <a href=\"https://spoonacular.com/recipes/vegetable-fried-brown-rice-36199\">Vegetable Fried Brown Rice</a>, <a href=\"https://spoonacular.com/recipes/vegetable-fried-cauliflower-rice-933261\">Vegetable Fried Cauliflower Rice</a>, and <a href=\"https://spoonacular.com/recipes/easy-vegetable-fried-brown-rice-with-egg-802042\">Easy Vegetable Fried Brown Rice with Egg</a>.",
-    "instructions": "<ol><li><span></span>Remove the cauliflower's tough stem and reserve for another use. Using a food processor, pulse cauliflower florets until they resemble rice or couscous. You should end up with around four cups of \"cauliflower rice.\"</li><li>Heat 1T butter and 1T oil in a large skillet over medium heat. Add garlic and the white and light green pieces of scallion. Sauté about a minute.</li><li>Add the cauliflower to the pan. Stir to coat with oil, then spread out in pan and let sit; you want it cook a bit and to caramelize (get a bit brown), which will bring out the sweetness. After a couple of minutes, stir and spread out again.</li><li>Add cold rice (it separates easily, so it won't clump up during cooking), plus the additional grapeseed and coconut oil or butter. Raise heat to medium-high. Toss everything together and, again, spread the mixture out over the whole pan and press a bit into the bottom. Let it sit for about two minutes—so the rice can get toasted and a little crispy. Add the peas and broccoli and stir again. Drizzle soy sauce and toasted sesame oil over rice.</li><li>Cook for another minute or so and turn off heat. Add chopped scallion tops and toss.</li><li>I like to toast some sesame seeds in a dry pan; I sprinkle these and some more raw, chopped scallion over the top of the rice for added flavor and crunch.</li><li>Season to taste with salt and, if you'd like, more soy sauce. Keep in mind that if you're serving this with something salty and saucy (ie. teriyaki chicken) you may want to hold off on adding too much salt to the fried rice.</li></ol>",
-    "sourceUrl": "http://fullbellysisters.blogspot.com/2012/01/cauliflower-fried-rice-more-veggies.html",
-    "ingredients": [
-        {
-            "id": "11090",
-            "name": "broccoli",
-            "amount": 2.0,
-            "unit": "cups"
-        },
-        {
-            "id": "11135",
-            "name": "cauliflower",
-            "amount": 1.0,
-            "unit": "head"
-        },
-        {
-            "id": "4047",
-            "name": "coconut oil",
-            "amount": 1.0,
-            "unit": ""
-        },
-        {
-            "id": "20041",
-            "name": "cooked brown rice",
-            "amount": 3.0,
-            "unit": "cups"
-        },
-        {
-            "id": "11215",
-            "name": "garlic",
-            "amount": 5.0,
-            "unit": "cloves"
-        },
-        {
-            "id": "4517",
-            "name": "grapeseed oil",
-            "amount": 1.0,
-            "unit": ""
-        },
-        {
-            "id": "16424",
-            "name": "low sodium soy sauce",
-            "amount": 3.0,
-            "unit": "T"
-        },
-        {
-            "id": "11304",
-            "name": "peas",
-            "amount": 1.0,
-            "unit": "cup"
-        },
-        {
-            "id": "2047",
-            "name": "salt",
-            "amount": 8.0,
-            "unit": "servings"
-        },
-        {
-            "id": "11291",
-            "name": "scallion",
-            "amount": 8.0,
-            "unit": "servings"
-        },
-        {
-            "id": "11291",
-            "name": "scallions",
-            "amount": 7.0,
-            "unit": ""
-        },
-        {
-            "id": "4058",
-            "name": "sesame oil",
-            "amount": 2.0,
-            "unit": "t"
-        },
-        {
-            "id": "12023",
-            "name": "sesame seeds",
-            "amount": 8.0,
-            "unit": "servings"
-        }
-    ]
-}"""
-    private val jsonObject = JSONObject(json)
-
     lateinit var imageView: ImageView
     lateinit var titleTextView: TextView
     lateinit var timeTextView: TextView
@@ -124,74 +35,85 @@ class RecipeDetailsActivity : AppCompatActivity() {
     lateinit var recipeDetailsToolbar: MaterialToolbar
     lateinit var favoritesButton: ImageButton
 
+    var id: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_details)
 
         connectViews()
-        val recipe = getRecipe()
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://glacial-lake-83393.herokuapp.com/recipe/" + this.id.toString()
 
-        Picasso.get().load(recipe.image).into(imageView)
-        titleTextView.text = recipe.title
-        timeTextView.text = "${recipe.readyInMinutes} minutes"
-        servingsTextView.text = "${recipe.servings} servings"
-        summaryTextView.text = getHtmlText(recipe.summary)
-        instructionTextView.text = getHtmlText(recipe.instructions)
-        recipeDetailsToolbar.title = recipe.title
-        favoritesButton.setImageResource(
-            if (FavoritesList.contains(recipe.id)) R.drawable.ic_round_favorite_24
-            else R.drawable.ic_round_favorite_border_24
-        )
-        resourceLinkButton.setOnClickListener {
-            val uri: Uri = Uri.parse(recipe.sourceUrl)
-            val intent = Intent(Intent.ACTION_VIEW, uri)
-            startActivity(intent)
-        }
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val jsonObject = JSONObject(response)
+                val ingredients = mutableListOf<IngredientDTO>()
+                val ingredientsJsonArray = jsonObject.getJSONArray("ingredients")
+                for (i: Int in 0 until ingredientsJsonArray.length()) {
+                    val ingredientJsonObject = ingredientsJsonArray.getJSONObject(i)
+                    ingredients.add(IngredientDTO(
+                        ingredientJsonObject.getInt("id"),
+                        ingredientJsonObject.getString("name"),
+                        ingredientJsonObject.getDouble("amount"),
+                        ingredientJsonObject.getString("unit")
+                    ))
+                }
 
-        favoritesButton.setOnClickListener {
-            if (FavoritesList.contains(recipe.id)) {
-                FavoritesList.delete(recipe.id)
-                favoritesButton.setImageResource(R.drawable.ic_round_favorite_border_24)
-            } else {
-                FavoritesList.add(recipe)
-                favoritesButton.setImageResource(R.drawable.ic_round_favorite_24)
-            }
-        }
+                val recipe = RecipeDTO(
+                    jsonObject.getInt("id"),
+                    jsonObject.getString("title"),
+                    jsonObject.getInt("readyInMinutes"),
+                    jsonObject.getInt("servings"),
+                    jsonObject.getString("image"),
+                    jsonObject.getString("summary"),
+                    jsonObject.getString("instructions"),
+                    jsonObject.getString("sourceUrl"),
+                    ingredients
+                )
 
-        populateIngredientsList(recipe)
+                Picasso.get().load(recipe.image).into(imageView)
+                titleTextView.text = recipe.title
+                timeTextView.text = "${recipe.readyInMinutes} minutes"
+                servingsTextView.text = "${recipe.servings} servings"
+                summaryTextView.text = getHtmlText(recipe.summary)
+                instructionTextView.text = getHtmlText(recipe.instructions)
+                recipeDetailsToolbar.title = recipe.title
+                favoritesButton.setImageResource(
+                    if (FavoritesList.contains(recipe.id)) R.drawable.ic_round_favorite_24
+                    else R.drawable.ic_round_favorite_border_24
+                )
+                resourceLinkButton.setOnClickListener {
+                    val uri: Uri = Uri.parse(recipe.sourceUrl)
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }
 
-    }
+                favoritesButton.setOnClickListener {
+                    if (FavoritesList.contains(recipe.id)) {
+                        FavoritesList.delete(recipe.id)
+                        favoritesButton.setImageResource(R.drawable.ic_round_favorite_border_24)
+                    } else {
+                        FavoritesList.add(recipe)
+                        favoritesButton.setImageResource(R.drawable.ic_round_favorite_24)
+                    }
+                }
 
-    private fun getRecipe(): RecipeDTO {
-        val ingredients = mutableListOf<IngredientDTO>()
-        val ingredientsJsonArray = jsonObject.getJSONArray("ingredients")
-        for (i: Int in 0 until ingredientsJsonArray.length()) {
-            val ingredientJsonObject = ingredientsJsonArray.getJSONObject(i)
-            ingredients.add(IngredientDTO(
-                ingredientJsonObject.getInt("id"),
-                ingredientJsonObject.getString("name"),
-                ingredientJsonObject.getDouble("amount"),
-                ingredientJsonObject.getString("unit")
-            ))
-        }
+                populateIngredientsList(recipe)
+            },
+            {
+                // that didn't work
+            })
 
-        return RecipeDTO(
-            jsonObject.getInt("id"),
-            jsonObject.getString("title"),
-            jsonObject.getInt("readyInMinutes"),
-            jsonObject.getInt("servings"),
-            jsonObject.getString("image"),
-            jsonObject.getString("summary"),
-            jsonObject.getString("instructions"),
-            jsonObject.getString("sourceUrl"),
-            ingredients
-        )
+        queue.add(stringRequest)
     }
 
 
     private fun connectViews() {
         val extras = intent.extras
         if (extras == null) finish()
+        this.id = extras!!.getInt("id")
 
         imageView = findViewById(R.id.recipe_details_img)
         titleTextView = findViewById(R.id.recipe_details_title)
--- frontend/app/src/main/java/com/tochanenko/recipefinder/SearchFragment.kt
+++ frontend/app/src/main/java/com/tochanenko/recipefinder/SearchFragment.kt
@@ -5,72 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
 
 class SearchFragment : Fragment() {
-
-    private val json = """
-        {"recipes": [
-                {
-                    "id": 716426,
-                    "title": "Cauliflower, Brown Rice, and Vegetable Fried Rice",
-                    "image": "https://spoonacular.com/recipeImages/716426-312x231.jpg"
-                },
-                {
-                    "id": 715594,
-                    "title": "Homemade Garlic and Basil French Fries",
-                    "image": "https://spoonacular.com/recipeImages/715594-312x231.jpg"
-                },
-                {
-                    "id": 715497,
-                    "title": "Berry Banana Breakfast Smoothie",
-                    "image": "https://spoonacular.com/recipeImages/715497-312x231.jpg"
-                },
-                {
-                    "id": 644387,
-                    "title": "Garlicky Kale",
-                    "image": "https://spoonacular.com/recipeImages/644387-312x231.jpg"
-                },
-                {
-                    "id": 716268,
-                    "title": "African Chicken Peanut Stew",
-                    "image": "https://spoonacular.com/recipeImages/716268-312x231.jpg"
-                },
-                {
-                    "id": 716381,
-                    "title": "Nigerian Snail Stew",
-                    "image": "https://spoonacular.com/recipeImages/716381-312x231.jpg"
-                },
-                {
-                    "id": 782601,
-                    "title": "Red Kidney Bean Jambalaya",
-                    "image": "https://spoonacular.com/recipeImages/782601-312x231.jpg",
-                    "imageType": "jpg"
-                },
-                {
-                    "id": 794349,
-                    "title": "Broccoli and Chickpea Rice Salad",
-                    "image": "https://spoonacular.com/recipeImages/794349-312x231.jpg"
-                },
-                {
-                    "id": 715446,
-                    "title": "Slow Cooker Beef Stew",
-                    "image": "https://spoonacular.com/recipeImages/715446-312x231.jpg"
-                },
-                {
-                    "id": 715415,
-                    "title": "Red Lentil Soup with Chicken and Turnips",
-                    "image": "https://spoonacular.com/recipeImages/715415-312x231.jpg"
-                }
-            ]
-        }
-    """.trimIndent()
-    private val jsonObject = JSONObject(json)
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -84,30 +30,41 @@ class SearchFragment : Fragment() {
     }
 
     private fun populateRecipesList(view: View) {
-        val recipesJsonList: JSONArray = jsonObject.getJSONArray("recipes")
-        val recipes = mutableListOf<Recipe>()
+        val queue = Volley.newRequestQueue(view.context)
+        val url = "https://glacial-lake-83393.herokuapp.com/recipe?number=5&offset=0"
 
-        for (i: Int in 0 until recipesJsonList.length()) {
-            val recipeJsonObject = recipesJsonList.getJSONObject(i)
-            recipes.add(
-                Recipe(
-                    recipeJsonObject.getInt("id"),
-                    recipeJsonObject.getString("image"),
-                    recipeJsonObject.getString("title")
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response -> val recipesJsonList: JSONArray = JSONObject(response).getJSONArray("recipes")
+                val recipes = mutableListOf<Recipe>()
+
+                for (i: Int in 0 until recipesJsonList.length()) {
+                    val recipeJsonObject = recipesJsonList.getJSONObject(i)
+                    recipes.add(
+                        Recipe(
+                            recipeJsonObject.getInt("id"),
+                            recipeJsonObject.getString("image"),
+                            recipeJsonObject.getString("title")
+                        )
+                    )
+                }
+
+                val recipesList = view.findViewById<View>(R.id.recipes_list) as RecyclerView
+
+                val adapter = RecipesListAdapter(recipes)
+                recipesList.adapter = adapter
+                recipesList.addItemDecoration(
+                    DividerItemDecoration(
+                        recipesList.context,
+                        DividerItemDecoration.VERTICAL
+                    )
                 )
-            )
-        }
+                recipesList.layoutManager = LinearLayoutManager(activity)
+            },
+            {
+                // that didn't work
+            })
 
-        val recipesList = view.findViewById<View>(R.id.recipes_list) as RecyclerView
-
-        val adapter = RecipesListAdapter(recipes)
-        recipesList.adapter = adapter
-        recipesList.addItemDecoration(
-            DividerItemDecoration(
-                recipesList.context,
-                DividerItemDecoration.VERTICAL
-            )
-        )
-        recipesList.layoutManager = LinearLayoutManager(activity)
+        queue.add(stringRequest)
     }
 }
